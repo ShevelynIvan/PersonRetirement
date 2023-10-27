0007eb17fdd89e882f52e860726596b51a054c99
ShevelynIvan <ivan.shevelyn1@gmail.com>
2023-10-27 14:32:18 +0300

    Добавьте файлы проекта.

---
+++ PersonRetirement/Person.cs
@@ -0,0 +1,67 @@
+﻿namespace PersonRetirement
+{
+    public class Person
+    {
+        private int _age;
+        private int _experience;
+
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Age. Can't be more than 120.
+        /// </summary>
+        public int Age 
+        {
+            get => _age;
+            set => _age = Validation(value, 120);
+        }
+
+        /// <summary>
+        /// Number of years of work experience. Can't be more than (age - 16).
+        /// </summary>
+        public int Experience 
+        {
+            get => _experience;
+            set => _experience = Validation(value, _age - 16);
+        }
+
+        /// <summary>
+        /// Constructor to create the person
+        /// </summary>
+        /// <param name="name">Name of person</param>
+        /// <param name="age">Age of person</param>
+        /// <param name="experience">Work experience (number of years)</param>
+        public Person(string name, int age, int experience)
+        {
+            Name = name;
+            Age = age;
+            Experience = experience;
+        }
+
+        /// <summary>
+        /// Calculates retirement value in other ways
+        /// </summary>
+        /// <param name="func">Func delegate to calculate retirement value</param>
+        /// <returns>Retirement value</returns>
+        public double GetRetirementValue(Func<int, int, double> func)
+        {
+            return func(Age, Experience);
+        }
+
+        /// <summary>
+        /// Validation method to set age and experience in right way
+        /// </summary>
+        /// <param name="value">Value to set</param>
+        /// <param name="maxPossibleValue">Max possible value to set</param>
+        /// <returns></returns>
+        private int Validation(int value, int maxPossibleValue)
+        {
+            if (value < 0)
+                return 0;
+            if (value > maxPossibleValue)
+                return maxPossibleValue;
+
+            return value;
+        }
+    }
+}
---
+++ PersonRetirement/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace PersonRetirement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person alex = new Person("Alex", 65, 35);
+            Person olha = new Person("Olha", 57, 40);
+            Person dima = new Person("Dima", 20, 0);
+
+            Console.WriteLine($"Retirement value for {alex.Name}");
+            Console.WriteLine($"UA in uah: {alex.GetRetirementValue(GetRetirementValueUA)}");
+            Console.WriteLine($"PL in uah: {alex.GetRetirementValue(GetRetirementValuePL)}");
+            Console.WriteLine($"GB in uah: {alex.GetRetirementValue(GetRetirementValueGB)}");
+
+            Console.WriteLine($"Retirement value for {olha.Name}");
+            Console.WriteLine($"UA in uah: {olha.GetRetirementValue(GetRetirementValueUA)}");
+            Console.WriteLine($"PL in uah: {olha.GetRetirementValue(GetRetirementValuePL)}");
+            Console.WriteLine($"GB in uah: {olha.GetRetirementValue(GetRetirementValueGB)}");
+
+            Console.WriteLine($"Retirement value for {dima.Name}");
+            Console.WriteLine($"UA in uah: {dima.GetRetirementValue(GetRetirementValueUA)}");
+            Console.WriteLine($"PL in uah: {dima.GetRetirementValue(GetRetirementValuePL)}");
+            Console.WriteLine($"GB in uah: {dima.GetRetirementValue(GetRetirementValueGB)}");
+        }
+
+        static double GetRetirementValueUA(int age, int exp)
+        {
+            if (age < 60)
+                return 0;
+
+            if (exp < 16)
+                return 2400;
+
+            return age * exp * 4;
+        }
+
+        static double GetRetirementValuePL(int age, int exp)
+        {
+            if (age < 55)
+                return 0;
+
+            if (exp < 16)
+                return 3200;
+
+            return age * exp * 5;
+        }
+
+        static double GetRetirementValueGB(int age, int exp)
+        {
+            if (age < 55)
+                return 0;
+
+            if (exp < 21)
+                return 4100;
+
+            return age * exp * 6;
+        }
+    }
+}
